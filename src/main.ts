698b030ce1314b15a9d260c83bbc606ed24f951d
vinicius silva <vinicius_dev@hotmail.com>
2023-07-12 22:28:03 -0300

    chore: pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import './assets/main.css'
 
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.mount('#app')
